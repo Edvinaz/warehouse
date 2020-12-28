52fe718a31dcd2565527bc04d9b8c22625525bbc
Edvinas Pinkevičius <edvinas.pinkevicius@gmail.com>
2020-12-28 10:05:24 +0200

    access denied handler

---
+++ src/Security/AccessDeniedHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Security;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        // ...
+        $content = 'content';
+
+        return new Response($content, 403);
+    }
+}
